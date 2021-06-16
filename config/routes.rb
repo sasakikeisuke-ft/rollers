1b5796a83084c9cc039ea8aaa1e03797118d6f10
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-16 23:23:17 +0900

    テスト用のコントローラーを作成
    ログイン機能のチェックのため、テスト用のコントローラーを作成しました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'tests#index'
+  get 'tests/index'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
