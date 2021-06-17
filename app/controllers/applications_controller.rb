2c5ae77188f827e2b24465fcf35af8af1bc646b2
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-17 15:12:29 +0900

    ルーティング設定
    ルーティングを設定しました。
    また不必要になったtestコントローラーを削除しました。

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -4,6 +4,21 @@ class ApplicationsController < ApplicationController
     @applications = Application.all
   end
 
-  
+  def new
+    @application = Application.new
+  end
 
+def create
+    @application = Application.new(application_params)
+    if @application.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def application_params
+    params.require(:application).permit(:カラム名, :カラム名).merge(user_id: current_user.id)
+  end
 end
