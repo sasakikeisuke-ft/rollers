99c61c7499c5287906b1fa5f6424dff4c7e83391
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-17 11:27:05 +0900

    gemfileテーブルのファイル作成

---
+++ spec/factories/gemfiles.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :gemfile do
+    
+  end
+end
---
+++ spec/models/gemfile_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Gemfile, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
