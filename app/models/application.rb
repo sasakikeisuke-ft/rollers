1e90c58edf48270c525e7cb4157e9c3cafce2eee
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-17 11:52:06 +0900

    バリデーションとアソシエーションを記載
    applicationsテーブルとgemfilesテーブルのバリデーションとアソシエーションを記載しました。

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,2 +1,7 @@
 class Application < ApplicationRecord
+
+  validates :application_name, presence: true
+
+  belongs_to :user
+  has_one :gemfile
 end
--- app/models/gemfile.rb
+++ app/models/gemfile.rb
@@ -1,2 +1,17 @@
 class Gemfile < ApplicationRecord
+
+  with_options presence: true do
+    validates :devise
+    validates :pry_rails
+    validates :image_magick
+    validates :active_hash
+    validates :rails_i18n
+    validates :ransack
+    validates :rubocop
+    validates :rspec
+    validates :payjp
+    validates :s3
+  end
+
+  belongs_to :application
 end
