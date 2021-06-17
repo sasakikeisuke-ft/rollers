c4a72b3f1b1f8c84713e0f60bd54e6341b15dc64
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-17 11:20:17 +0900

    applicationsテーブルのマイグレーションファイル編集

--- db/migrate/20210617021534_create_applications.rb
+++ db/migrate/20210617021534_create_applications.rb
@@ -1,7 +1,9 @@
 class CreateApplications < ActiveRecord::Migration[6.0]
   def change
     create_table :applications do |t|
-
+      t.string :application_name, null: false
+      t.text :application_description
+      t.references :user, foreign_key: true
       t.timestamps
     end
   end
