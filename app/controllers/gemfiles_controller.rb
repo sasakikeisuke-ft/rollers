9f49c093216bd02b84fe5df6ff3adc99d16986d8
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-18 14:59:12 +0900

    new, create, edit, updateアクションを記載

---
+++ app/controllers/gemfiles_controller.rb
@@ -0,0 +1,39 @@
+class GemfilesController < ApplicationController
+  before_action :find_gemfile, only: [:edit, :update]
+
+  def new
+    @gemfile = Gemfile.new
+  end
+
+  def create
+    @gemfile = Gemfile.new(gemfile_params)
+    if @gemfile.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @gemfile.update(gemfile_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def gemfile_params
+    params.require(:gemfile).permit(:devise, :pry_rails,:image_magick, :active_hash, :rails_i18n, :ransack, :rubocop, :rspec, :payjp, :s3).merge(application_id: params[:application_id])
+  end
+
+  def find_gemfile
+    @application = Application.find(params[:application_id])
+    @gemfile = @application.gemfile
+    redirect_to root_path if current_user.id != @application.user_id
+  end
+
+end
