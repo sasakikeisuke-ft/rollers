38b4a98194cace71d8a149782554e3d52830da31
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-18 14:58:25 +0900

    gemfileコントローラーへのルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: 'applications#index'
   devise_for :users
-  resources :applications
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :applications do
+    resources :gemfiles, only: [:new, :create, :edit, :update]
+  end
+  
 end
