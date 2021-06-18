90896a1b0bcbaf1b794801d9668dcbf58c300e2b
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-18 17:04:47 +0900

    コード一覧へのルーティングとリンクを設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: 'applications#index'
   devise_for :users
   resources :applications do
-    resources :gemfiles, only: [:new, :create, :edit, :update]
+    resources :gemfiles, only: [:index, :new, :create, :edit, :update]
   end
   
 end
