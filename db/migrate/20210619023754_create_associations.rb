b2e23a9695f9ab516738991cd99122b28bea2516
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-19 22:21:31 +0900

    修正：マイグレーションファイルを方向転換前へ
    マイグレーションファイルは変更する必要がない可能性があり、現在のところはこのまま進めてみます。
    問題が明確になったら修正案を考えます。

--- db/migrate/20210619023754_create_associations.rb
+++ db/migrate/20210619023754_create_associations.rb
@@ -1,9 +1,10 @@
 class CreateAssociations < ActiveRecord::Migration[6.0]
   def change
     create_table :associations do |t|
-      t.references :model, foreign_key: true
-      t.integer :pair_id, null: false
+      t.integer :left, null: false
+      t.integer :right, null: false
       t.integer :relation_id, null: false
+      t.references :application, foreign_key: true
       t.timestamps
     end
   end
