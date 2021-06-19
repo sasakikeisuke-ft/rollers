823ed7ea98c9a9f3f00509ec7bbf913195c27927
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-19 22:22:52 +0900

    association#newの追加設定
    登録モデルの数が1以下の場合、association#newアクションをスキップしcolumns#newへ遷移するように設定しました。

--- app/controllers/associations_controller.rb
+++ app/controllers/associations_controller.rb
@@ -1,7 +1,7 @@
 class AssociationsController < ApplicationController
 
   def new
-  #   @models = Model.where(application_id: params[:application_id])
-   redirect_to new_application_model_column_path(params[:application_id], params[:model_id])
+    @models = Model.where(application_id: params[:application_id])
+    redirect_to new_application_model_column_path(params[:application_id], params[:model_id]) if @models.length <= 1
   end
 end
