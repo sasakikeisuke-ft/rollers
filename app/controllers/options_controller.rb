73eef9b74e6694de7a17c16407c5bfe377f6a533
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-26 19:26:23 +0900

    オプション作成機能を作成
    オプションを作成する機能を作成しました。
    データが保存できることを確認した段階です。

--- app/controllers/options_controller.rb
+++ app/controllers/options_controller.rb
@@ -1,7 +1,25 @@
 class OptionsController < ApplicationController
+  before_action :find_option, only: [:edit, :update, :destroy]
 
-  
+  def new
+    @column = Column.find(params[:column_id])
+    @options = @column.options
+    @option = Option.new
+  end
 
+  def create
+    @option = Option.new(option_params)
+    if @option.save
+      
+      redirect_to new_application_model_column_option_path(params[:application_id], params[:model_id], params[:column_id])
+    else
+      render :new
+    end
+  end
 
+  private
+  def option_params
+    params.require(:option).permit(:option_type_id, :input1, :input2).merge(column_id: params[:column_id])
+  end
 
 end
