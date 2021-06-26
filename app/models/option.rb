5b893d1d853e93d2b54259d7be514d0dbc858604
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-26 12:22:32 +0900

    optionモデル、ActiveHash ;option_typeを編集

---
+++ app/models/option.rb
@@ -0,0 +1,9 @@
+class Option < ApplicationRecord
+  validates :option_type_id, numericality: { other_than: 0, message: "を選択してください" }
+
+  belongs_to :column
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :option_type
+
+end
