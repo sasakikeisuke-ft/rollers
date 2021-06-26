d8139347055b0fea38a0a7a1a96f1e02edbf9b19
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-26 16:26:26 +0900

    カラム編集機能の実装
    カラム編集画面へのリンクを設定し、編集が行えるように実装しました。

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,4 +1,5 @@
 class ColumnsController < ApplicationController
+  before_action :find_column, only: [:edit, :update, :destroy]
 
   def new
     @model = Model.find(params[:model_id])
@@ -15,9 +16,27 @@ class ColumnsController < ApplicationController
     end
   end
 
+  def edit
+    @model = Model.find(params[:model_id])
+  end
+
+  def update
+    if @column.update(column_params)
+      redirect_to application_models_path(params[:application_id])
+    else
+      @model = Model.find(params[:model_id])
+      render :edit
+    end
+  end
+
   private
   def column_params
     params.require(:column).permit(:name, :name_ja, :data_type_id, :must_exsit, :unique).merge(model_id: params[:model_id])
+  end
+
+  def find_column
+    @column = Column.find(params[:id])
+    redirect_to root_path if current_user.id != @column.model.application.user_id
   end
 
 end
