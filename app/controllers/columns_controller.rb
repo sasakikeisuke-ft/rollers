99095a965db0671a55f49a3edb665a7f5af94292
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-27 22:57:15 +0900

    モデル作成画面の編集

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -3,6 +3,7 @@ class ColumnsController < ApplicationController
   before_action :find_column, only: [:edit, :update, :destroy]
 
   def new
+    @models = Model.where(application_id: @application)
     @model = Model.find(params[:model_id])
     @column = Column.new
   end
@@ -10,7 +11,7 @@ class ColumnsController < ApplicationController
   def create
     @column = Column.new(column_params)
     if @column.save
-      redirect_to new_application_model_column_path(params[:application_id], params[:model_id])
+      redirect_to new_application_model_column_option_path(params[:application_id], params[:model_id], @column)
     else
       @model = Model.find(params[:model_id])
       render :new
