270b1a32729e60df76ef0f2f880618b81e2bb4bf
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-07-01 16:39:00 +0900

    コード表示に使用しているメソッドを共有化
    - 対象モデルで最も長いカラム名の文字数を算出するメソッド
    - 指定した回数だけ半角スペースを挿入するメソッド
    - 指定した回数だけハイフンを挿入するメソッド
    上記三つのメソッドを共有化しました。

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # 半角スペースを指定した回数だけ挿入するメソッド
+  def insert_space(roops)   
+    space = '' 
+    if roops >= 1 
+      roops.times do  
+        space += '&nbsp;' 
+      end 
+    end 
+   return raw(space) 
+ end 
+
 end
--- app/helpers/gemfiles_helper.rb
+++ app/helpers/gemfiles_helper.rb
@@ -1,2 +1,36 @@
 module GemfilesHelper
+  
+  # 対象モデルの中で最も長いカラム名の文字数を返すメソッド
+  def the_longest(longest ,model)
+    longest = 18 if model.model_type_id == 5  
+    model.columns.each do |column| 
+      long = column.name.length if column.data_type_id != 13 
+      long = column.name.length + 3 if column.data_type_id == 13 
+      longest = long if long > longest 
+    end 
+    return longest
+  end
+
+  # 指定した回数だけ - を挿入するメソッド
+  def insert_bar(roops)
+    bar = ''
+    roops.times do
+    bar += '-'
+    end
+    return raw(bar)
+  end
+
+  # 上記の the_longest の一部変更したメソッド
+  # モデルではなくアプリケーション全体から、最も長いカラム名の文字数を返す。
+  def longest_of_all(columns)
+    longest = 6 if model.model_type_id != 5  
+    longest = 18 if model.model_type_id == 5  
+    columns.each do |column| 
+      long = column.name.length if column.data_type_id != 13 
+      long = column.name.length + 3 if column.data_type_id == 13 
+      longest = long if long > longest   
+    end
+  end
+    
+
 end
