b8cad3541a0b6ed94ec8754c4ccaf5d6136fd5c1
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-07-18 19:42:27 +0900

    controllerモデルファイルの作成

--- app/models/controller.rb
+++ app/models/controller.rb
@@ -1,2 +1,18 @@
 class Controller < ApplicationRecord
+  with_options presence: true do
+    validates :nameuniqueness: { scope: :application_id, message: "はすでに存在します" }
+    with_options format: { with: /\A[0-9]+\z/, message: 'は半角数字を入力してください' },
+                           numericality: { other_than: 0, message: "を選択してください" } do
+      validates :index
+      validates :new
+      validates :create
+      validates :edit
+      validates :update
+      validates :destroy
+      validates :show
+    end
+  end
+
+  belongs_to :application
+  has_many :actions
 end
--- spec/factories/controllers.rb
+++ spec/factories/controllers.rb
@@ -1,5 +1,4 @@
 FactoryBot.define do
   factory :controller do
-    
   end
 end
