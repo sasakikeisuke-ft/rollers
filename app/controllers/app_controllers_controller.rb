08de531e2f8d31bda053f0e8d5aa93422f3c1819
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-07-19 22:29:46 +0900

    コントローラー新規登録機能を実装

--- app/controllers/app_controllers_controller.rb
+++ app/controllers/app_controllers_controller.rb
@@ -1,8 +1,38 @@
 class AppControllersController < ApplicationController
+  before_action :get_common_matter, only: [:new, :edit]
+  before_action :find_app_controller, only: [:edit, :update, :destroy]
 
   def index
     @application = Application.find(params[:application_id])
     @app_controllers = @application.app_controllers
+  end
+
+  def new
+    get_common_matter
+    @app_controller = AppController.new
+  end
+
+  def create
+    @app_controller = AppController.new(app_controller_params)
+    if @app_controller.save
+      redirect_to application_app_controllers_path
+    else
+      get_common_matter
+      render :new
+    end
+  end
+
+  private
+
+  def get_common_matter
+    @application = Application.find(params[:application_id])
+    @models = @application.models
+    @app_controllers = @application.app_controllers
+  end
+
+  def app_controller_params
+    params.require(:app_controller).permit(:name, :parent, :index_select, :new_select, :create_select, :edit_select, :update_select, 
+                                       :destroy_select, :show_select).merge(application_id: params[:application_id])
   end
 
 end
