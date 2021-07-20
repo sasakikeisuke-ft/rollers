76d9a320c770aae329f7ff14642f5f6fa8ece8b9
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-07-20 21:50:26 +0900

    コントローラーのコード表示機能に関するヘルパーメソッドを一部編集
    フォーム作成に関連するインスタンス変数を取得できるコードを作成するために、ヘルパーメソッドを編集しました。

--- app/helpers/app_controllers_helper.rb
+++ app/helpers/app_controllers_helper.rb
@@ -2,16 +2,23 @@ module AppControllersHelper
 
   # コントローラーのコードを作成するメインのメソッド。ここから複数のメソッドを起動する。
   def make_controller_html(app_controller)
-    contents = {}
+    contents = {
+      before_find_count: 0,
+      before_common_count: 0
+    }
 
+    # app_controller.actions.each do |action|
 
+    if app_controller.create_select || app_controller.update
+      contents[:params] = true
+    end
 
-    make_params_html(app_controller, contents)
+    make_params_html(app_controller, contents) if contents[:params]
     make_find_html(app_controller, contents)
     contents
   end
 
-  
+
 
 
   def make_params_html(app_controller, contents)
@@ -57,10 +64,44 @@ module AppControllersHelper
   end
 
   def make_find_html(app_controller, contents)
-    html = "#{insert_space(2)}find_#{insert_space(2)}def #{app_controller.name}<br>"
+    parents = @app_controller.application.app_controllers
+    array = []
+    make_find_array(app_controller, parents, array)
+    
+    html = "#{insert_space(2)}def find_#{app_controller.name}<br>"
     html += "#{insert_space(4)}@#{app_controller.name} = #{app_controller.name.classify}.find(params[:id])<br>"
+    array.each do |element|
+      html += "#{insert_space(4)}@#{element[:parent]} = @#{element[:child]}.#{element[:parent]}<br>"
+    end
     html += "#{insert_space(2)}end<br><br>"
     contents[:find_model] = html
+  end
+
+  def make_find_array(app_controller, parents, array)
+    unless app_controller.parent == ''
+      parents.each do |parent|
+        if parent.name == app_controller.parent
+          content = {
+            child: app_controller.name,
+            parent: parent.name
+          }
+          array << content
+          make_find_array(parent, parents, array)
+        end
+      end
+    end
+  end
+
+  # 失敗作。スコープの問題だと思うが原因がわかっていない。後学のため残してく。
+  def add_find_html(app_controller, parents, html)
+    if app_controller.parent != ''
+      parents.each do |parent|
+        if parent.name == app_controller.parent
+          html += "#{insert_space(4)}@#{parent.name} = @#{app_controller.name}.#{parent.name}<br>"
+         add_find_html(parent, parents, html) 
+        end
+      end
+    end
   end
 
 end
