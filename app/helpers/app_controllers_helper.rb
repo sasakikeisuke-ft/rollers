7cec6de4eac9e2dd2a5321525cdf6ee7213dffda
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-07-24 12:06:42 +0900

    ヘルパーメソッドにおける変数名を変更
    ストロングパラメーター取得メソッド作成メソッドにおける変数名をわかりやすいものへと変更しました。

--- app/helpers/app_controllers_helper.rb
+++ app/helpers/app_controllers_helper.rb
@@ -1,6 +1,10 @@
 module AppControllersHelper
 
-  # コントローラーのコードを作成するメインのメソッド。ここから複数のメソッドを起動する。
+  # コントローラーのHTMLを作成するメソッドを起動するメソッド。
+  #   ストロングパラメーター取得メソッドのHTMLを作成するメソッド
+  #   create/edit/update/showで共通するfindを作成するメソッド
+
+  # コントローラーのHTMLを作成するメソッドを起動するメソッド。
   def make_controller_html(app_controller)
     contents = {
       before_find_count: 0,
@@ -18,50 +22,56 @@ module AppControllersHelper
     contents
   end
 
+  def count_action(app_controller)
+    
+  end
 
 
-
+  # ストロングパラメーター取得メソッドに関するHTMLを作成するメソッド
   def make_params_html(app_controller, contents)
     models = app_controller.application.models
     model = models.includes(:columns).find_by(name: app_controller.name)
-    devise = models.find_by(model_type_id: 5) # devise対応のモデル
-    references_array = []
-    activehash_array = []
-    normarl_array = []
+    devise = models.find_by(model_type_id: 5) # devise対応のモデルを取得
+    
+    # データ型によって配列を振り分ける。
+    references_columns = []
+    activehash_columns = []
+    normarl_columns = []
     model.columns.each do |column|
       case column.data_type_id
-      when 12
-        references_array << column.name
-      when 13
-        activehash_array << column.name
+      when 12 # references型のカラム
+        references_columns << column.name
+      when 13 # ActiveHashを参照するカラム
+        activehash_columns << column.name
       else
-        normarl_array << column.name
+        normarl_columns << column.name
       end
     end
+    # 振り分けられた配列をもとにHTMLを作成
     html = "#{insert_space(2)}def #{app_controller.name}_params<br>"
     html += "#{insert_space(4)}params.require(:#{model.name}).permit("
     first = true
-    normarl_array.each do |element|
+    normarl_columns.each do |column|
       html += ', ' unless first
-      html += ":#{element}"
+      html += ":#{column}"
       first = false
     end
-    activehash_array.each do |element|
+    activehash_columns.each do |column|
       html += ', ' unless first
-      html += ":#{element}_id"
+      html += ":#{column}_id"
       first = false
     end
     html += ')'
-    if references_array.length != 0
+    if references_columns.length != 0
       first = true
       html += '.merge('
-      references_array.each do |element|
+      references_columns.each do |column|
         html += ', ' unless first
-        if element != devise.name
-          html += "#{element}_id: params[:#{element}]"
+        if column != devise.name
+          html += "#{column}_id: params[:#{column}]"
           first = false
         else
-          html += "#{element}_id: current_user.id"
+          html += "#{column}_id: current_user.id"
           first = false
         end
       end
@@ -70,6 +80,7 @@ module AppControllersHelper
     contents[:params] = html
   end
 
+  # create/edit/update/showで共通するfindを作成するメソッド
   def make_find_html(app_controller, contents)
     html = "#{insert_space(2)}def find_#{app_controller.name}<br>"
     html += "#{insert_space(4)}@#{app_controller.name} = #{app_controller.name.classify}.find(params[:id])<br>"
