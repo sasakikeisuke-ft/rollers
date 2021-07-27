cd0ce662c8f95be429ae4cc5da61ecefee8eca33
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-07-27 11:40:34 +0900

    actionsコントローラーを作成

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -1,2 +1,49 @@
 class ActionsController < ApplicationController
+  before_action :find_action, only: [:edit, :update, :destroy]
+  before_action :instance_variable_for_form, only: [:new, :edit]
+
+  def new
+    @action = Action.new
+  end
+
+  def create
+    @action = Action.new(action_params)
+    if @action.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @action.update
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @action.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def action_params
+    params.require(:action).permit(:target, :input1, :input2, :input3, :action_type_id, :aciton_code_id).merge(app_controller_id: params[:app_controller])
+  end
+
+  def find_action
+    @action = Action.find(params[:id])
+  end
+
+  def instance_variable_for_form
+    @app_controller = @action.app_controller
+    @application = @app_controller.application
+  end
+
 end
