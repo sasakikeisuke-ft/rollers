1e5ec498f9423b5d6cd461b03f0dc7a9d24b3e8e
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-07-27 22:05:41 +0900

    app_actionのモデルファイルの編集

--- app/models/app_action.rb
+++ app/models/app_action.rb
@@ -1,2 +1,13 @@
 class AppAction < ApplicationRecord
+  validates :target, presence: true
+  with_options numericality: { other_than: 0, message: " can't be blank" } do
+    validates :action_type_id
+    validates :code_type_id
+  end
+
+  belongs_to :app_controller
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :action_type
+  belongs_to :code_type
 end
