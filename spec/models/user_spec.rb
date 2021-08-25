eeee250cfd199e824d2df50cabcedd7b3be39aab
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-08-25 20:03:01 +0900

    deviseに関するモデルのテストコードを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,68 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.build(:user)
+    sleep 0.1 # エラーを防ぐために休止時間を入れています。不要なら削除してください。
+  end
+
+  describe 'Userモデルの登録機能' do
+    context '入力内容が適切であれば登録できる' do
+      it '入力内容が全て適切であれば登録できる' do
+        expect(@user).to be_valid
+      end
+      it 'passwordが英数字混合の6文字であれば登録できる' do
+        @user.password = 'a12345'
+        @user.password_confirmation = @user.password
+        expect(@user).to be_valid
+      end
+    end
+
+    context '不適切な内容があり登録できない' do
+      it 'nicknameが空欄だと登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("ニックネームを入力してください")
+      end
+      it 'emailが空欄だと登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Eメールを入力してください')
+      end
+      it 'emailに@が含まれていない場合、登録できない' do
+        @user.email = 'abcdefgh'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Eメールは不正な値です')
+      end
+      it 'emailの重複があり登録できない' do
+        @user.save
+        another_user = FactoryBot.build(:user, email: @user.email)
+        another_user.valid?
+        expect(another_user.errors.full_messages).to include('Eメールはすでに存在します')
+      end
+      it 'passwordが空欄だと登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("パスワードを入力してください")
+      end
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = 'abcdef'
+        @user.password_confirmation = 'ghijkl'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワード（確認用）とパスワードの入力が一致しません')
+      end
+      it '5文字以下の場合、登録できない' do
+        @user.password = '1abcd'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは6文字以上で入力してください')
+      end
+      it '全角文字では登録できない' do
+        @user.password = 'ひらがなカタカナ漢字'
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('パスワードは英字と数字の両方を含む必要があります')
+      end
+    end
+  end
 end
